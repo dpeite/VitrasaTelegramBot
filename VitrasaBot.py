fb79dcd0a563087ddcb3ef0de6076d3440264682
Daniel Vilar <danielvilar2@gmail.com>
2017-06-29 23:22:35 +0200

    First commit

--- VitrasaBot.py
+++ VitrasaBot.py
@@ -1,9 +1,54 @@
-# -*- coding: utf-8 -*-   
-import telebot
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask import request, abort
 from api import vitrasa
 
-token = ""
-bot = telebot.TeleBot(token)
+import telebot
+
+
+API_TOKEN = ''
+
+WEBHOOK_HOST = '.appspot.com'
+WEBHOOK_PORT = 443  # 443, 80, 88 or 8443 (port need to be 'open')
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
+
+bot = telebot.TeleBot(API_TOKEN, threaded=False)
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
+
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH, certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello world!'
+
+
+# Process webhook calls
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        abort(403)
 
 
 def obtener_parada(message, id):
@@ -67,5 +112,4 @@ def coordenadas_parada(message):
          bot.send_message(message.chat.id, "{}".format(e.message))
     except Exception:
         bot.send_message(message.chat.id, "Se ha producido un error al comunicarse con Vitrasa")
-         
-bot.polling(none_stop=True)
+
