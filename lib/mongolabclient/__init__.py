f9db66ed1e8fc2f4141b995c6beecb3afff0bbb9
Daniel Vilar <danielvilar2@gmail.com>
2019-12-31 12:40:07 +0100

    Remove unused libraries

--- lib/mongolabclient/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 *-*
-"""HTTP requests implementation for `MongoLab REST API`_
-
-.. _MongoLab REST API: http://support.mongolab.com/entries/20433053-rest-api-for-mongodb"""
-
-from lib.mongolabclient.client import MongoLabClient
